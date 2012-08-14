fc5130034166a4428582610332e9301cef6418b0
mi nombre <jperez@i-360.net>
2012-08-14 14:36:27 -0430

    subiendo proyecto sin historial

---
+++ app/view/Categorias.js
@@ -0,0 +1,61 @@
+Ext.define('Contact',{
+	extend: 'Ext.data.Model',
+	config: {
+		fields: ['valor']
+	}
+});
+
+var datos = Ext.create('Ext.data.Store', {
+	storeId: 'ListStore',
+	id: 'ListStore',
+	model: 'Contact',
+	sorters: 'valor',
+	grouper: function(record){
+		return record.get('valor')[0];
+	},
+	data: [
+		{valor: 'Accesorios para Vehículos'},
+		{valor: 'Animales y Mascotas'},
+		{valor: 'Bebés'},
+		{valor: 'Cámaras y Accesorios'},
+		{valor: 'Celulares y Teléfonos'},
+		{valor: 'Coleccionables y Hobbies'},
+		{valor: 'Computación'},
+		{valor: 'Consolas y Videojuegos'},
+		{valor: 'Deportes y Fitness'},
+		{valor: 'Electrodomésticos'},
+		{valor: 'Electrónica,  Audio y Video'},
+		{valor: 'Hogar y Muebles'},
+		{valor: 'Industrias'},
+		{valor: 'Instrumentos Musicales'},
+		{valor: 'Juegos y Juguetes'},
+		{valor: 'Libros, Música y Películas'},
+		{valor: 'Relojes, Joyas y Bisutería'},
+		{valor: 'Ropa, Zapatos y Accesorios'},
+		{valor: 'Salud y Belleza'},
+		{valor: 'Otras Categorías'}
+	]
+});
+
+Ext.define("retobb.view.Categorias", {
+    extend: 'Ext.Panel',
+    xtype: 'tipoCategorias',
+    config: {
+    	items: [
+		{
+		xtype: 'toolbar',
+		docked: 'top',
+		items: [{ xtype: 'spacer'},
+			{ xtype: 'searchfield', placeHolder: 'Filtrar'},
+			{ xtype: 'spacer'}]
+		},
+		{
+		xtype: 'list',
+		width:'100',
+		height:650,
+		itemTpl: '<div class="contact">{valor}</div>',
+		store: 'ListStore'
+		}
+	]
+    }
+});
---
+++ app/view/Main.js
@@ -0,0 +1,104 @@
+Ext.define("retobb.view.Main", {
+    extend: 'Ext.tab.Panel',
+    requires: [
+        'Ext.TitleBar',
+        'Ext.Panel',
+        'Ext.field.Search',
+        'Ext.List',
+	'Ext.form.FieldSet',
+	'Ext.SegmentedButton',
+	'Ext.dataview.NestedList',
+	'Ext.form.Panel'
+    ],
+    config: {
+        tabBarPosition: 'bottom',
+	layout: {
+		animation: {
+			type: 'fade'
+		}
+	},
+
+        items: [
+            {
+		xtype: 'tipoCategorias',
+                title: 'Categorias',
+                iconCls: 'more',
+            },
+            {
+		xtype: 'nestedlist',
+                title: 'Actividades',
+                iconCls: 'time'
+            },
+	    {
+		xtype: 'panel',
+	    	title: 'Buscar',
+		iconCls: 'search',
+		layout: 'vbox',
+	    	items: [
+			{title: 'detalle', html: 'detalle de producto', style: 'color:white; background-color: black', height: 100},
+			{
+				xtype: 'toolbar',
+				items: [
+					{ xtype: 'spacer'},
+					{
+						xtype: 'segmentedbutton',
+						align:'center',
+						allowDepress: false,
+						items: [
+							{text:'Ver todos', pressed: true},
+							{text:'Sólo amigos'}
+						]
+					},
+					{ xtype: 'spacer'}
+				]
+			},
+			{
+				flex: 4,
+				xtype: 'panel',
+				layout: 'hbox',
+				defaults: {flex: 1},
+
+				items: [
+					{title: 'pros',html: 'lista', style: 'background-color:#3b7e00'},
+					{title: 'cons',html: 'listado', style: 'background-color:#b22222'}
+				]
+
+			}
+		]
+	    },
+	    {
+	    	title: 'Preferencias',
+		iconCls: 'settings'
+	    },
+	    {
+		xtype: 'formpanel',
+	    	title: 'Cuenta',
+		iconCls: 'user',
+		url: 'tusabes.php',
+		layout: 'vbox',
+
+		items: [
+			{
+				xtype: 'fieldset',
+				title: 'Contactanos',
+				instructions: '(La direccion de correo es opcional)',
+				items: [
+					{xtype: 'textfield', label: 'Nombre'},
+					{xtype: 'textfield', label: 'Correo'},
+					{xtype: 'textareafield', label: 'Mensaje'}
+				],
+			
+			},
+			{
+				xtype: 'button',
+				text: 'enviar',
+				ui: 'confirm',
+				handler: function(){
+					this.up('formpanel').submit();
+				}
+			}
+		]
+	    }
+        ]
+    }
+});
